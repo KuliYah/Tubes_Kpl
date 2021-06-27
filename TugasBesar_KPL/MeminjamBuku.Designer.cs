12d03f75f0cc3c9f92845bd8a243fff380ac9f16
xikdp <dandakomang09@gmail.com>
2021-06-27 14:21:06 +0700

    Update MeminjamBuku.Designer.cs
    update meminjam buku

--- TugasBesar_KPL/MeminjamBuku.Designer.cs
+++ TugasBesar_KPL/MeminjamBuku.Designer.cs
@@ -39,6 +39,11 @@ namespace TugasBesar_KPL
             this.labelJudulBuku = new System.Windows.Forms.Label();
             this.btnSubmit = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.LibraryId = new System.Windows.Forms.ColumnHeader();
+            this.NamaPeminjam = new System.Windows.Forms.ColumnHeader();
+            this.IdBuku = new System.Windows.Forms.ColumnHeader();
+            this.JudulBuku = new System.Windows.Forms.ColumnHeader();
+            this.TglPinjam = new System.Windows.Forms.ColumnHeader();
             this.SuspendLayout();
             // 
             // tbLibraryId
@@ -123,18 +128,55 @@ namespace TugasBesar_KPL
             // 
             // listView1
             // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.LibraryId,
+            this.NamaPeminjam,
+            this.IdBuku,
+            this.JudulBuku,
+            this.TglPinjam});
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(26, 269);
+            this.listView1.Location = new System.Drawing.Point(54, 249);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(121, 97);
+            this.listView1.Size = new System.Drawing.Size(704, 158);
             this.listView1.TabIndex = 9;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // LibraryId
+            // 
+            this.LibraryId.Name = "LibraryId";
+            this.LibraryId.Text = "Library Id";
+            this.LibraryId.Width = 100;
+            // 
+            // NamaPeminjam
+            // 
+            this.NamaPeminjam.Name = "NamaPeminjam";
+            this.NamaPeminjam.Text = "Nama Peminjam";
+            this.NamaPeminjam.Width = 150;
+            // 
+            // IdBuku
+            // 
+            this.IdBuku.Name = "IdBuku";
+            this.IdBuku.Text = "Id Buku";
+            this.IdBuku.Width = 100;
+            // 
+            // JudulBuku
+            // 
+            this.JudulBuku.Name = "JudulBuku";
+            this.JudulBuku.Text = "JudulBuku";
+            this.JudulBuku.Width = 200;
+            // 
+            // TglPinjam
+            // 
+            this.TglPinjam.Name = "TglPinjam";
+            this.TglPinjam.Text = "Tanggal Pinjam";
+            this.TglPinjam.Width = 150;
             // 
             // MeminjamBuku
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(811, 419);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.btnSubmit);
@@ -167,5 +209,10 @@ namespace TugasBesar_KPL
         private System.Windows.Forms.Label labelJudulBuku;
         private System.Windows.Forms.Button btnSubmit;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader LibraryId;
+        private System.Windows.Forms.ColumnHeader NamaPeminjam;
+        private System.Windows.Forms.ColumnHeader IdBuku;
+        private System.Windows.Forms.ColumnHeader JudulBuku;
+        private System.Windows.Forms.ColumnHeader TglPinjam;
     }
 }
